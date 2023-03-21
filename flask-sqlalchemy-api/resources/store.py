05cdcdd90646a7edc688dd24e5f12c94b220e406
Utkarsh Singh <Utkarsh.Singh@watchguard.com>
2023-03-21 17:17:11 +0530

    Use flask-sqlalchemy for refactoring flask-smorest-api

---
+++ flask-sqlalchemy-api/resources/store.py
@@ -0,0 +1,47 @@
+from db import db
+from flask import request
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+from models import StoreModel
+from resources.schemas import StoreSchema
+from sqlalchemy.exc import IntegrityError, SQLAlchemyError
+
+blp = Blueprint("stores", __name__, description="Operations on stores")
+
+
+@blp.route("/store/<string:store_id>")
+class Store(MethodView):
+    @blp.response(200, StoreSchema)
+    def get(self, store_id):
+        store = StoreModel.query.get_or_404(store_id)
+        return store
+
+    def delete(self, store_id):
+        store = StoreModel.query.get_or_404(store_id)
+        db.session.delete(store)
+        db.session.commit()
+        return {"message": "Store deleted"}, 200
+
+
+@blp.route("/store")
+class StoreList(MethodView):
+    @blp.response(200, StoreSchema(many=True))
+    def get(self):
+        return StoreModel.query.all()
+
+    @blp.arguments(StoreSchema)
+    @blp.response(201, StoreSchema)
+    def post(self, store_data):
+        store = StoreModel(**store_data)
+        try:
+            db.session.add(store)
+            db.session.commit()
+        except IntegrityError:
+            abort(
+                400,
+                message="A store with that name already exists.",
+            )
+        except SQLAlchemyError:
+            abort(500, message="An error occurred creating the store.")
+
+        return store
