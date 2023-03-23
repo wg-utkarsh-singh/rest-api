1ec01ce40c25eca7c58ca6bf2506c472cdd94dc9
Utkarsh Singh <Utkarsh.Singh@watchguard.com>
2023-03-23 11:49:42 +0530

    Add flask-migration-api

---
+++ flask-migrate-api/resources/user.py
@@ -0,0 +1,100 @@
+from hashlib import sha256
+
+from db import db
+from flask.views import MethodView
+from flask_jwt_extended import (
+    create_access_token,
+    create_refresh_token,
+    get_jwt_identity,
+    get_jwt,
+    jwt_required,
+)
+from flask_smorest import Blueprint, abort
+from models import UserModel
+from resources.schemas import UserSchema
+
+from blocklist import BLOCKLIST
+
+blp = Blueprint("Users", "users", description="Operations on users")
+
+
+def hash_password(password):
+    return sha256(password.encode("utf-8")).hexdigest()
+
+
+@blp.route("/register")
+class UserRegister(MethodView):
+    @blp.arguments(UserSchema)
+    def post(self, user_data):
+        if UserModel.query.filter(UserModel.username == user_data["username"]).first():
+            abort(409, message="A user with that username already exists.")
+
+        user = UserModel(
+            username=user_data["username"],
+            password=hash_password(user_data["password"]),
+        )
+        db.session.add(user)
+        db.session.commit()
+
+        return {"message": "User created successfully."}, 201
+
+
+@blp.route("/user")
+class UserList(MethodView):
+    @blp.response(200, UserSchema(many=True))
+    def get(self):
+        return UserModel.query.all()
+
+
+@blp.route("/user/<int:user_id>")
+class User(MethodView):
+    @blp.response(200, UserSchema)
+    def get(self, user_id):
+        user = UserModel.query.get_or_404(user_id)
+        return user
+
+    def delete(self, user_id):
+        user = UserModel.query.get_or_404(user_id)
+        db.session.delete(user)
+        db.session.commit()
+        return {"message": "User deleted."}, 200
+
+
+@blp.route("/login")
+class UserLogin(MethodView):
+    @blp.arguments(UserSchema)
+    def post(self, user_data):
+        user = UserModel.query.filter(
+            UserModel.username == user_data["username"]
+        ).first()
+
+        if user and hash_password(user_data["password"]) == user.password:
+            access_token = create_access_token(identity=user.id, fresh=True)
+            refresh_token = create_refresh_token(user.id)
+            return {"access_token": access_token, "refresh_token": refresh_token}, 200
+
+        abort(401, message="Invalid credentials.")
+
+
+@blp.route("/logout")
+class UserLogout(MethodView):
+    @jwt_required()
+    def post(self):
+        jti = get_jwt()["jti"]
+        BLOCKLIST.add(jti)
+        return {"message": "Successfully logged out"}, 200
+
+
+@blp.route("/refresh")
+class TokenRefresh(MethodView):
+    @jwt_required(refresh=True)
+    def post(self):
+        current_user = get_jwt_identity()
+        new_token = create_access_token(identity=current_user, fresh=False)
+
+        # Make it clear that when to add the refresh token to the blocklist
+        # will depend on the app design
+        jti = get_jwt()["jti"]
+        BLOCKLIST.add(jti)
+
+        return {"access_token": new_token}, 200
